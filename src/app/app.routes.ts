faf37d4014c5748d18f504daf99bb4eb7099673e
cheikh tidiane <cheikh951812@gmail.com>
2024-05-22 00:41:48 +0200

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { TrainingComponent } from './components/training/training.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent, title: 'Home Page !'},
+    {path: 'signup', component: SignupComponent, title: 'Signup'},
+    {path: 'login', component: LoginComponent, title: 'Login'},
+    {path: 'training', component: TrainingComponent, title: 'Training'}
+];
