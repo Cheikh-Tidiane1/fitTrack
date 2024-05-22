b638e5e4c0bdffe660c3e1b9b6f3ed5d0c3d0ff6
cheikh tidiane <cheikh951812@gmail.com>
2024-05-22 23:50:05 +0200

    feat: signup layout

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FlexLayoutModule} from '@angular/flex-layout';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
