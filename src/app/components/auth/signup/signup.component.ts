3d48889e5141ccfe127e735d85488066e775aaf3
cheikh tidiane <cheikh951812@gmail.com>
2024-06-03 23:50:21 +0200

    feat: button submit

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -3,10 +3,11 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule],
+  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,MatButtonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
