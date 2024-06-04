bd3eadab0fe18d72bcdb8f40db76fc7e672b23cf
cheikh tidiane <cheikh951812@gmail.com>
2024-06-04 23:39:37 +0200

    feat: configure submit button

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,MatButtonModule],
+  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,MatButtonModule,FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+  }
 
 }
