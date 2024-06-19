fd9a42e0c3e8d02002fe07748aec02f2519ae5f9
cheikh tidiane <cheikh951812@gmail.com>
2024-06-19 22:57:59 +0200

    feat: add datePicker

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ChangeDetectionStrategy } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import { FormsModule, NgForm } from '@angular/forms';
+import {provideNativeDateAdapter} from '@angular/material/core';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,MatButtonModule,FormsModule],
+  providers: [provideNativeDateAdapter()],
+  imports: [MatSelectModule,MatInputModule,MatFormFieldModule,FlexLayoutModule,MatButtonModule,FormsModule,MatDatepickerModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
