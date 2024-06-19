312bc6850a8cad0d847a66ba8737c8df1066a3bc
cheikh tidiane <cheikh951812@gmail.com>
2024-06-19 22:59:58 +0200

    feat: add internationalisation fr-Fr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(),{provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}]
 };
